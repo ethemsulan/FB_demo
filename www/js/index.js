18fcb97161a15cae8cfcc5e04d97aefde6731c70
Ethem Sulan <ethemsulan@gmail.com>
2014-12-29 16:10:36 +0200

    add push

--- www/js/index.js
+++ www/js/index.js
@@ -417,35 +417,39 @@ var app = {
 		console.log("mapLoaded");
 		app.detectCurrentLocation();
 	},
-	loadPushNot: function initPushwoosh(){
+	loadPushNot:function initPushwoosh() {
         var pushNotification = window.plugins.pushNotification;
      
-        //set push notifications handler
+        //set push notification callback before we initialize the plugin
         document.addEventListener('push-notification', function(event) {
-            var title = event.notification.title;
-            var userData = event.notification.userdata;
-                                     
-            if(typeof(userData) != "undefined") {
-                console.warn('user data: ' + JSON.stringify(userData));
-            }
-                                         
-            alert(title);
-        });
+                                    //get the notification payload
+                                    var notification = event.notification;
      
-        //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_NUMBER", appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
-        pushNotification.onDeviceReady({ projectid: "561798195163", appid : "13FDF-795B4" });
+                                    //display alert to the user for example
+                                    alert(notification.aps.alert);
+                                   
+                                    //clear the app badge
+                                    pushNotification.setApplicationIconBadgeNumber(0);
+                                });
      
+        //initialize the plugin
+        pushNotification.onDeviceReady({pw_appid:"13FDF-795B4"});
+         
         //register for pushes
         pushNotification.registerDevice(
             function(status) {
-                var pushToken = status;
-                console.warn('push token: ' + pushToken);
+                var deviceToken = status['deviceToken'];
+                console.warn('registerDevice: ' + deviceToken);
             },
             function(status) {
-                console.warn(JSON.stringify(['failed to register ', status]));
+                console.warn('failed to register : ' + JSON.stringify(status));
+                alert(JSON.stringify(['failed to register ', status]));
             }
         );
+         
+        //reset badges on app start
+        pushNotification.setApplicationIconBadgeNumber(0);
     }
-    
+    	
 };
 
